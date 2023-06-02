be7bd38c36fa66a10814063b2d0711bf219c5cce
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-06-01 20:10:44 -0500

    Update conection.js

--- Client-side/unionseguros/public/scripts/conection.js
+++ Client-side/unionseguros/public/scripts/conection.js
@@ -1,2 +1,2 @@
-const GLOBAL_URL = 'http://localhost:8080/api/v1'
-// const GLOBAL_URL = 'http://54.208.54.180:8080/api/v1'
+//const GLOBAL_URL = 'http://localhost:8080/api/v1'
+const GLOBAL_URL = 'http://54.208.54.180:8080/api/v1'
