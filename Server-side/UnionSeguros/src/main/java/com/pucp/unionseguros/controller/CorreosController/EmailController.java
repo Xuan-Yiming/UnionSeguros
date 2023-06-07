60a351fbed80ce2b8d0e12c457892eb99eb8dbb3
SergioDadic <89951269+SergioDadic@users.noreply.github.com>
2023-06-06 23:49:37 -0500

    aaaaaaa

---
+++ Server-side/UnionSeguros/src/main/java/com/pucp/unionseguros/controller/CorreosController/EmailController.java
@@ -0,0 +1,36 @@
+package com.pucp.unionseguros.controller.CorreosController;
+
+import java.util.List;
+
+import com.pucp.unionseguros.service.CorreosService.EmailService;
+import org.apache.tomcat.websocket.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/email")
+@CrossOrigin
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    //Envia un email dado 3 cadenas, mensaje, email destino y asunto
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    boolean sendEmail(@RequestBody List<String> correo)throws AuthenticationException{
+        boolean success = false;
+
+        try{
+            success = emailService.sendEmailTool(correo.get(0),correo.get(1),correo.get(2));
+        }
+        catch(Exception ex){ System.out.print(ex.getMessage()); }
+
+        return success;
+    }
+
+}
