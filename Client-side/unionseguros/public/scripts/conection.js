2eac11e811d87b4e6c05f869d5071271df8bdc46
SergioDadic <89951269+SergioDadic@users.noreply.github.com>
2023-06-06 23:50:39 -0500

    aaaaaaa

--- Client-side/unionseguros/public/scripts/conection.js
+++ Client-side/unionseguros/public/scripts/conection.js
@@ -1,3 +1,3 @@
-//const GLOBAL_URL = 'http://localhost:8080/api/v1'
-const GLOBAL_URL = 'http://54.208.54.180:8080/api/v1'
+const GLOBAL_URL = 'http://localhost:8080/api/v1'
+// const GLOBAL_URL = 'http://54.208.54.180:8080/api/v1'
 
