6e5570b2227f1ad01122d09234558389f64bd44d
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-06-08 19:20:41 -0500

    seguroVehicular

---
+++ Client-side/unionseguros/public/scripts/admin/adminSeguroVehicular.js
@@ -0,0 +1,205 @@
+var beneficios;
+var searchTimer;
+window.onload = function () {
+  if (localStorage.getItem("user") == null) {
+    window.location.href = "/admin/login";
+  }
+
+  fetch(GLOBAL_URL + "/detalleCotizacion/listarTodosActivos")
+    .then((response) => response.json())
+    .then((data) => {
+      this.beneficios = data;
+      crearLaTabla(data);
+    })
+    .catch((error) => {
+      // Handle the error
+      console.error(error);
+    });
+
+//   document.querySelector("#txt-buscar").addEventListener("input", function () {
+//     clearTimeout(searchTimer);
+//     searchTimer = setTimeout(function () {
+//       const query = document.querySelector("#txt-buscar").value.toLowerCase();
+
+//       let params = new URLSearchParams();
+//       params.append("busqueda", query);
+
+//       let url = new URL(
+//         GLOBAL_URL + "/planSOAT/buscarbeneficiosSOAT?" + params.toString()
+//       );
+
+//       fetch(url)
+//         .then((response) => response.json())
+//         .then((data) => {
+//           this.beneficios = data;
+//           crearLaTabla(data);
+//         })
+//         .catch((error) => {
+//           // Handle the error
+//           console.error(error);
+//         });
+//     }, 500);
+//   });
+
+  document.querySelector("#btn-nuevo").addEventListener("click", function () {
+    localStorage.removeItem("data-plan");
+    openModal(configurarModal());
+  });
+    
+    document.querySelector("#btn-modal-guardar").addEventListener("click", function () {
+        closeModal(function () {
+            const beneficio = document.querySelector("#txt-beneficio").value;
+            const monto = document.querySelector("#txt-monto").value;
+            const data = {
+                beneficio: beneficio,
+                monto: monto,
+                activo: true
+            };
+            if (localStorage.getItem("data-plan")) {
+                const dataId = JSON.parse(localStorage.getItem("data-plan")).id;
+                data.id = dataId;
+                fetch(GLOBAL_URL + "/detalleCotizacion/modificar", {
+                  method: "PUT",
+                  body: JSON.stringify(data),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                })
+                  .then((response) => response.json())
+                  .then((element) => {
+                    if (element) {
+                      alert("Se ha actualizado correctamente");
+                      location.reload();
+                      return true;
+                    } else {
+                      alert("No se ha podido actualizar");
+                      return false;
+                    }
+                  })
+                  .catch((error) => {
+                    // Handle the error
+                    console.error(error);
+                  });
+            } else {
+                fetch(GLOBAL_URL + "/detalleCotizacion/insertar", {
+                  method: "POST",
+                  body: JSON.stringify(data),
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                })
+                  .then((response) => response.json())
+                  .then((element) => {
+                    if (element) {
+                      alert("Se ha insertado correctamente");
+                      location.reload();
+                      return true;
+                    } else {
+                      alert("No se ha podido insertar");
+
+                      return false;
+                    }
+                  })
+                  .catch((error) => {
+                    // Handle the error
+                    console.error(error);
+                  });
+            }
+        });
+    });
+
+    document
+      .querySelector("#btn-modal-cancelar")
+      .addEventListener("click", function () {
+        closeModal(function () {
+          return true;
+        });
+      });
+};
+
+function configurarModal() {
+  if (localStorage.getItem("data-plan")) {
+    const data = JSON.parse(localStorage.getItem("data-plan"));
+    document.querySelector("#txt-beneficio").value = data.beneficio;
+    document.querySelector("#txt-monto").value = data.monto;
+  } else {
+    document.querySelector("#txt-beneficio").value = "";
+    document.querySelector("#txt-monto").value = "";
+  }
+}
+
+function crearLaTabla(data) {
+  const table = document.querySelector("#table-body");
+  table.innerHTML = "";
+  data.forEach((beneficio) => {
+    const tableRow = document.createElement("tr");
+    tableRow.classList.add("table-row");
+
+    const ID = document.createElement("td");
+    ID.classList.add("td-id");
+    ID.innerText = beneficio.id;
+    tableRow.appendChild(ID);
+
+    const descripcionBenficio = document.createElement("td");
+    descripcionBenficio.classList.add("td-beneficio");
+    descripcionBenficio.innerText = beneficio.beneficio;
+    tableRow.appendChild(descripcionBenficio);
+
+    const monto = document.createElement("td");
+    monto.classList.add("td-monto");
+    monto.innerText = beneficio.monto;
+    tableRow.appendChild(monto);
+
+    //add edit button
+    const button = document.createElement("td");
+    const editButton = document.createElement("button");
+    editButton.classList.add("btn-edit");
+    editButton.innerText = "Editar";
+    editButton.setAttribute("data-id", beneficio.id);
+    editButton.addEventListener("click", () => {
+      const dataId = event.target.getAttribute("data-id");
+      localStorage.setItem(
+        "data-plan",
+        JSON.stringify(beneficios.find((beneficio) => beneficio.id == dataId))
+      );
+      openModal(configurarModal());
+    });
+    button.appendChild(editButton);
+
+    //add delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn-delete");
+    deleteButton.innerText = "Eliminar";
+    deleteButton.setAttribute("data-id", beneficio.id);
+    deleteButton.addEventListener("click", () => {
+      const dataId = event.target.getAttribute("data-id");
+      if (
+        confirm(
+          "¿Está seguro que desea eliminar el plan SOAT con ID: " + dataId + "?"
+        )
+      ) {
+        fetch(GLOBAL_URL + "/detalleCotizacion/eliminar?idIngresado=" + dataId, {
+          method: "PUT",
+        })
+          .then((response) => response.json())
+          .then((element) => {
+            if (element) {
+              alert("Se ha eliminado correctamente");
+              location.reload();
+            } else {
+              alert("No se ha podido eliminar");
+              return;
+            }
+          })
+          .catch((error) => {
+            // Handle the error
+            console.error(error);
+          });
+      }
+    });
+    button.appendChild(deleteButton);
+    tableRow.appendChild(button);
+
+    table.appendChild(tableRow);
+  });
+}
