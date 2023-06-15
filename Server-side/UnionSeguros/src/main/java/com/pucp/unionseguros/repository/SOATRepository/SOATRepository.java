202c3e12be5714d6285bf2048291700d936f1418
JarumyN <a20181245@pucp.edu.pe>
2023-06-14 22:01:45 -0500

    BuscarSOAT

--- Server-side/UnionSeguros/src/main/java/com/pucp/unionseguros/repository/SOATRepository/SOATRepository.java
+++ Server-side/UnionSeguros/src/main/java/com/pucp/unionseguros/repository/SOATRepository/SOATRepository.java
@@ -2,6 +2,7 @@ package com.pucp.unionseguros.repository.SOATRepository;
 
 import com.pucp.unionseguros.model.SOAT.SOAT;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,11 @@ import java.util.List;
 public interface SOATRepository extends JpaRepository<SOAT,Integer> {
 
     public List<SOAT> findSOATSByActivoIsTrue();
+
+    @Query("SELECT s FROM SOAT s INNER JOIN Poliza po INNER JOIN Vehiculo v INNER JOIN Cliente c INNER JOIN PlanSOAT p " +
+            "WHERE CONCAT(s.id, c.nombre, c.apellidoPaterno, c.apellidoMaterno, v.placa, p.nombrePlan) " +
+            "LIKE CONCAT('%', ?1, '%') AND s.activo = true " +
+            "AND po.id = s.fidPoliza.id AND po.fidCliente.id = c.id AND po.fidVehiculo.id = v.id AND s.fidPlanSoat.id = p.id " +
+            "ORDER BY s.fechaDeEmision ASC ")
+    public List<SOAT> findSOATParametros(String busqueda);
 }
