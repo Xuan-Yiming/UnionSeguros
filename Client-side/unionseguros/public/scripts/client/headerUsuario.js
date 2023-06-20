fbc8459ce356a91cb2e59ae109397916eb5473f3
omaruiz2000 <a20185464@pucp.edu.pe>
2023-06-20 03:23:19 -0500

    EditarDatosPersonalesCompleto

--- Client-side/unionseguros/public/scripts/client/headerUsuario.js
+++ Client-side/unionseguros/public/scripts/client/headerUsuario.js
@@ -1,5 +1,3 @@
-window.onload = function () {
-    const userJSON = localStorage.getItem("userCliente");
-    const user = JSON.parse(userJSON);
-    document.querySelector('#group-user-button-text').textContent = (user.nombre + " " + user.apellidoPaterno).toUpperCase();
-}
+const userJSON = localStorage.getItem("userCliente");
+const user = JSON.parse(userJSON);
+document.querySelector('#group-user-button-text').textContent = (user.nombre + " " + user.apellidoPaterno).toUpperCase();
