36a81f07c8f7420c2e7e62ab46a28781e3ed5b36
SergioDadic <89951269+SergioDadic@users.noreply.github.com>
2023-06-24 22:06:12 -0500

    con auditoria una parte

---
+++ Server-side/UnionSeguros/src/main/java/com/pucp/unionseguros/model/Extras/Auditoria.java
@@ -0,0 +1,32 @@
+package com.pucp.unionseguros.model.Extras;
+import com.pucp.unionseguros.model.Personas.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "auditoria")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Auditoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_auditoria", nullable = false)
+    private Integer id;
+
+    @Column(name = "tiempo", length = 500)
+    private LocalDateTime tiempo;
+
+    @Column(name = "accion", length = 500)
+    private String accion;
+
+    @ManyToOne
+    @JoinColumn(name = "fid_usuario")
+    private Usuario fidUsuario;
+}
