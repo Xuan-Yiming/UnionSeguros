bf1f0ac746d09becdaa5f240c73ff58894ba8ae1
Xuan Yiming <XUANYIMING@OUTLOOK.COM.PE>
2023-06-24 19:57:07 -0500

    Update EmailXTokenController.java

--- Server-side/UnionSeguros/src/main/java/com/pucp/unionseguros/controller/ExtrasController/EmailXTokenController.java
+++ Server-side/UnionSeguros/src/main/java/com/pucp/unionseguros/controller/ExtrasController/EmailXTokenController.java
@@ -51,24 +51,24 @@ public class EmailXTokenController {
         return emailXTokenService.generarToken(emailXToken);
     }
 
-    @GetMapping("/validarToken")
-    public EmailXToken validarToken(@RequestParam(name = "email") String email,
-                                @RequestParam(name = "token") String token){
-        EmailXToken emailXToken=new EmailXToken();
-        emailXToken=null;
-        emailXToken=emailXTokenService.validarToken(email,token);
-        if(emailXToken==null){
-            emailXTokenService.resetearToken(email);
-        }
-        return emailXToken;
-    }
-
-    @GetMapping("/resetearToken")
-    public EmailXToken resetearToken(@RequestParam(name = "email") String email){
-        EmailXToken emailXToken=new EmailXToken();
-        emailXTokenService.resetearToken(email);
-        return emailXToken;
-    }
+//    @GetMapping("/validarToken")
+//    public EmailXToken validarToken(@RequestParam(name = "email") String email,
+//                                @RequestParam(name = "token") String token){
+//        EmailXToken emailXToken=new EmailXToken();
+//        emailXToken=null;
+//        emailXToken=emailXTokenService.validarToken(email,token);
+//        if(emailXToken==null){
+//            emailXTokenService.resetearToken(email);
+//        }
+//        return emailXToken;
+//    }
+//
+//    @GetMapping("/resetearToken")
+//    public EmailXToken resetearToken(@RequestParam(name = "email") String email){
+//        EmailXToken emailXToken=new EmailXToken();
+//        emailXTokenService.resetearToken(email);
+//        return emailXToken;
+//    }
 
     @GetMapping("/validarToken")
     public boolean validarToken(@RequestParam(name = "email") String email,
